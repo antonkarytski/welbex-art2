08ca98ca737c2ae9efb2270d9cea590a28518f3f
antonkarytski <heyheyjude23@gmail.com>
2022-12-17 22:52:25 +0100

    refactor: type naming in screen props

--- src/screens/profile/Screen.Profile.tsx
+++ src/screens/profile/Screen.Profile.tsx
@@ -1,9 +1,19 @@
-import { Text } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { useNavigate } from '../../navigation'
+import { links } from '../../navigation/links'
+import Span from '../../ui/Span'
 
 export default function ProfileScreen() {
-	return (
-		<Text>
-			Profile Screen
-		</Text>
-	)
+  const navigate = useNavigate()
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigate(links.editProfile)
+      }}
+    >
+      <Span>Profile Screen</Span>
+    </TouchableOpacity>
+  )
 }
