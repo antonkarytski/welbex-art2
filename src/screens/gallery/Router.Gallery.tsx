3bc7390045950dd8c54cba30d838667f936b6834
antonkarytski <heyheyjude23@gmail.com>
2022-12-19 18:05:19 +0100

    feat: add packages for top tab bar feat: add basic structure for gallery screen

--- src/screens/gallery/Router.Gallery.tsx
+++ src/screens/gallery/Router.Gallery.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import { GalleryIcon } from 'altek-ui'
+import TabStackNavigator from '../../navigation/elements/TabStackNavigator'
 import { createTabScreenDescription } from '../../navigation/helpers'
 import { links } from '../../navigation/links'
 import GalleryScreen from './Screen.Gallery'
@@ -10,13 +11,13 @@ const Stack = createNativeStackNavigator()
 
 const GalleryTabScreen = () => {
   return (
-    <Stack.Navigator>
+    <TabStackNavigator initialRoute={links.galleryMain}>
       <Stack.Screen name={links.galleryMain} component={GalleryScreen} />
       <Stack.Screen
         name={links.galleryFilter}
         component={GalleryFilterScreen}
       />
-    </Stack.Navigator>
+    </TabStackNavigator>
   )
 }
 
