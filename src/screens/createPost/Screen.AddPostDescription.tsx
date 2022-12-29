0d71a5086f91ce150b965cf57c916b360774ccb4
antonkarytski <heyheyjude23@gmail.com>
2022-12-29 19:46:23 +0100

    refactor: typing for Field

--- src/screens/createPost/Screen.AddPostDescription.tsx
+++ src/screens/createPost/Screen.AddPostDescription.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Image, ScrollView, StyleSheet, View } from 'react-native'
+import { createStateModel } from 'altek-toolkit'
 import ImagePreviewFormField from '../../features/createPost/ImagePreviewFormField'
 import { createPostFormModel } from '../../features/createPost/model'
+import BlockUploadFromCamera from '../../features/imagePick/Block.UploadFromCamera'
 import { createThemedStyle } from '../../features/themed'
 import { useThemedStyleList } from '../../features/themed/hooks'
+import { createSelectModel } from '../../lib/componentsModels/model.select'
 import ScreenHeader from '../../navigation/elements/ScreenHeader'
 import { primaryHeaderThemedStyles } from '../../navigation/elements/styles'
 import { links } from '../../navigation/links'
 import { RouterScreenProps } from '../../navigation/types.screenProps'
 import { useText } from '../../translations/hook'
 import Span from '../../ui/Span'
+import Select from '../../ui/dropdownSelect/DropdownSelect'
+import Field from '../../ui/form/Field'
+
+const m = createSelectModel()
 
 export default function AddPostDescriptionScreen({
   route,
@@ -33,6 +40,27 @@ export default function AddPostDescriptionScreen({
         formModel={createPostFormModel}
       />
       <Span label={text.completeDescription} />
+      <Field label={'title'} name={'title'} formModel={createPostFormModel} />
+      <Field
+        label={'category'}
+        name={'title'}
+        formModel={createPostFormModel}
+      />
+      <Field
+        disabled
+        label={'age'}
+        name={'title'}
+        formModel={createPostFormModel}
+      />
+      {/*<Select*/}
+      {/*  data={[{ a: 2 }, { a: 3 }]}*/}
+      {/*  renderItem={() => {*/}
+      {/*    return <Span label={'HELLO'} />*/}
+      {/*  }}*/}
+      {/*  idExtractorName={'a'}*/}
+      {/*  model={m}*/}
+      {/*/>*/}
+      <BlockUploadFromCamera />
     </ScrollView>
   )
 }
