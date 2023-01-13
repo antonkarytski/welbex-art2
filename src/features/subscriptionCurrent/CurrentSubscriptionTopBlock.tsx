98dd1f522bdf7bb9813f32a6d254cd76bb732fda
antonkarytski <heyheyjude23@gmail.com>
2023-01-14 00:30:52 +0100

    refactor: current subscription screen structure

--- src/features/subscriptionCurrent/CurrentSubscriptionTopBlock.tsx
+++ src/features/subscriptionCurrent/CurrentSubscriptionTopBlock.tsx
@@ -1,9 +1,81 @@
-import React from "react";
+import { useStore } from 'effector-react'
+import React from 'react'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { useNavigate } from '../../navigation'
+import { links } from '../../navigation/links'
+import { buttonCommonThemedPreset } from '../../styles/buttons'
+import { useText } from '../../translations/hook'
+import H2 from '../../ui/H2'
+import Span from '../../ui/Span'
+import RefreshIcon from '../../ui/icons/Icon.Refresh'
+import SubscriptionSelectItem from '../../ui/lists/SubscriptionSelectItem'
+import { $currentSubscription } from '../profile/model'
+import SubscriptionPlansTopBlock from '../subscriptionPlans/SubscriptionPlansTopBlock'
+import { getSubscriptionPriceText } from '../subscriptionPlans/helpers'
+import { subscriptionCurrentItemThemedStyles } from '../subscriptionPlans/styles'
+import { createThemedStyle } from '../themed'
+import { useThemedStyleList } from '../themed/hooks'
+import ChangeTariffButton from './ChangeTariffButton'
+import { currentSubscriptionControllersStyles } from './styles'
 
 type CurrentSubscriptionTopBlockProps = {}
 
 const CurrentSubscriptionTopBlock = ({}: CurrentSubscriptionTopBlockProps) => {
+  const text = useText()
+  const currentSubscription = useStore($currentSubscription)
+  const { styles, colors } = useThemedStyleList({
+    item: subscriptionCurrentItemThemedStyles,
+    common: themedStyles,
+  })
 
-};
+  if (!currentSubscription) return null
 
-export default CurrentSubscriptionTopBlock;
+  return (
+    <SubscriptionPlansTopBlock>
+      <H2 style={styles.common.header} label={text.myTariff} />
+      <View style={currentSubscriptionControllersStyles.container}>
+        <View style={currentSubscriptionControllersStyles.item}>
+          <SubscriptionSelectItem
+            style={styles.item}
+            value={currentSubscription.monthsAmount}
+            measure={text.months}
+            price={getSubscriptionPriceText(
+              currentSubscription.pricePerMonth,
+              text
+            )}
+          />
+        </View>
+        <ChangeTariffButton
+          style={styles.common.refreshButton}
+          textStyle={styles.common.refreshButtonText}
+          iconColor={colors.whiteText}
+        />
+      </View>
+    </SubscriptionPlansTopBlock>
+  )
+}
+
+const themedStyles = createThemedStyle((colors) =>
+  StyleSheet.create({
+    header: {
+      color: colors.whiteText,
+      marginLeft: 20,
+      marginBottom: 20,
+      marginTop: 24,
+    },
+    subscriptionControllersContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      paddingHorizontal: 10,
+      marginBottom: 26,
+    },
+    refreshButton: {
+      backgroundColor: colors.primary2,
+    },
+    refreshButtonText: {
+      color: colors.whiteText,
+    },
+  })
+)
+
+export default CurrentSubscriptionTopBlock
--- src/screens/subscription/Screen.CurrentSubscription.tsx
+++ src/screens/subscription/Screen.CurrentSubscription.tsx
@@ -1,33 +1,17 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { useStore } from 'effector-react'
-import moment from 'moment'
 import React, { useLayoutEffect } from 'react'
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { $currentSubscription } from '../../features/profile/model'
-import SubscriptionPlansTopBlock from '../../features/subscriptionPlans/SubscriptionPlansTopBlock'
-import { getSubscriptionPriceText } from '../../features/subscriptionPlans/helpers'
-import { subscriptionCurrentItemThemedStyles } from '../../features/subscriptionPlans/styles'
-import { createThemedStyle } from '../../features/themed'
-import { useThemedStyleList } from '../../features/themed/hooks'
+import CurrentSubscriptionContent from '../../features/subscriptionCurrent/CurrentSubscriptionContent'
+import CurrentSubscriptionTopBlock from '../../features/subscriptionCurrent/CurrentSubscriptionTopBlock'
 import { links } from '../../navigation/links'
 import { ScreensProps } from '../../navigation/types.screenProps'
-import { useText } from '../../translations/hook'
-import DataRow from '../../ui/DataRow'
-import H2 from '../../ui/H2'
-import Span from '../../ui/Span'
-import PresetButton from '../../ui/buttons/PresetButton'
-import RefreshIcon from '../../ui/icons/Icon.Refresh'
-import SubscriptionSelectItem from '../../ui/lists/SubscriptionSelectItem'
 
 const CurrentSubscriptionScreen = ({
   navigation,
 }: NativeStackScreenProps<ScreensProps, links.subscriptionSelectPlan>) => {
-  const text = useText()
   const currentSubscription = useStore($currentSubscription)
-  const { colors, styles } = useThemedStyleList({
-    item: subscriptionCurrentItemThemedStyles,
-    common: themedStyles,
-  })
 
   useLayoutEffect(() => {
     if (!currentSubscription) {
@@ -39,95 +23,18 @@ const CurrentSubscriptionScreen = ({
   if (!currentSubscription) return null
 
   return (
-    <View style={styles.common.container}>
-      <SubscriptionPlansTopBlock>
-        <H2 style={styles.common.header} label={text.myTariff} />
-        <View style={styles.common.subscriptionControllersContainer}>
-          <View style={styles.common.item}>
-            <SubscriptionSelectItem
-              style={styles.item}
-              value={currentSubscription.monthsAmount}
-              measure={text.months}
-              price={getSubscriptionPriceText(
-                currentSubscription.pricePerMonth,
-                text
-              )}
-            />
-          </View>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate(links.subscriptionSelectPlan)
-            }}
-            style={[styles.common.item, styles.common.refreshItem]}
-          >
-            <RefreshIcon color={colors.whiteText} />
-            <Span
-              weight={500}
-              style={styles.common.refreshItemText}
-              label={text.changeTariff}
-            />
-          </TouchableOpacity>
-        </View>
-      </SubscriptionPlansTopBlock>
-      <View style={styles.common.contentContainer}>
-        <DataRow
-          textStyle={styles.common.contentText}
-          title={`${text.subscriptionEndDate}:`}
-          value={moment(currentSubscription.expiresIn).format('DD.MM.YYYY')}
-        />
-        <PresetButton
-          style={{ marginTop: 'auto' }}
-          label={text.unsubscribe}
-          onPress={() => {}}
-        />
-      </View>
+    <View style={styles.container}>
+      <CurrentSubscriptionTopBlock />
+      <CurrentSubscriptionContent />
     </View>
   )
 }
 
-const themedStyles = createThemedStyle((colors) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingBottom: 45,
-    },
-    header: {
-      color: colors.whiteText,
-      marginLeft: 20,
-      marginBottom: 20,
-      marginTop: 24,
-    },
-    subscriptionControllersContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-      paddingHorizontal: 10,
-      marginBottom: 26,
-    },
-    item: {
-      flex: 1,
-      height: 152,
-      maxWidth: 200,
-      marginHorizontal: 10,
-    },
-    refreshItem: {
-      backgroundColor: colors.primary2,
-      borderRadius: 12,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    refreshItemText: {
-      color: colors.whiteText,
-      marginTop: 15,
-    },
-    contentContainer: {
-      paddingHorizontal: 20,
-      paddingTop: 24,
-      flex: 1,
-    },
-    contentText: {
-      color: colors.text,
-    },
-  })
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 45,
+  },
+})
 
 export default CurrentSubscriptionScreen
