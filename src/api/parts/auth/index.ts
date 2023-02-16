01d2f6af4f70e01da440ae2d020e3daa1702b08d
antonkarytski <heyheyjude23@gmail.com>
2023-02-16 11:54:12 +0100

    feat: add login logic

--- src/api/parts/auth/index.ts
+++ src/api/parts/auth/index.ts
@@ -2,8 +2,9 @@ import { ContentType } from '../../../lib/models/apiBuilder/types'
 import { apiManager } from '../../apiManager'
 import { LoginBody, LoginResponse } from './types'
 
-const auth = apiManager.endpoint('auth')
-const login = auth.post<LoginResponse, LoginBody>({
+const authEndpoint = apiManager.endpoint('auth')
+
+const login = authEndpoint.post<LoginResponse, LoginBody>({
   contentType: ContentType.FORM,
   endpoint: 'token',
 })
