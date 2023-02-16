005c8d43e29390118aaf54afbcd982fd5d533e44
antonkarytski <heyheyjude23@gmail.com>
2023-02-16 12:17:20 +0100

    refactor: styles in app

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import { NativeBaseProvider } from 'native-base'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import './src/api'
@@ -12,31 +13,9 @@ import Router from './src/screens/Router'
 export default function App() {
   const isLoaded = useCachedResources()
 
-  // useEffect(() => {
-  //   fetch('http://45.8.97.189/api/v1/users/me', {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization:
-  //         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo3LCJleHAiOjE2NzY1NDUzMjh9.E6UpI0BYWbYN4lq8zUt0NM8hEEn5TW0hRrPT9THrLMY',
-  //     },
-  //     method: 'GET',
-  //   })
-  //     .then((e) => {
-  //       console.log(e.headers)
-  //       e.json().then((e) => {
-  //         console.log(JSON.stringify(e))
-  //       })
-  //       console.log(e)
-  //       console.log('success')
-  //     })
-  //     .catch((e) => {
-  //       console.log(e)
-  //     })
-  // }, [])
-
   if (!isLoaded) return null
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={styles.container}>
       <NativeBaseProvider>
         <SafeAreaProvider>
           <NavigationContainer>
@@ -49,3 +28,7 @@ export default function App() {
     </GestureHandlerRootView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+})
