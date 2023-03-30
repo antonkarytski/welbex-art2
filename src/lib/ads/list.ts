a519238e83192859ec4bf7f5b8e9565bf9b63cd1
antonkarytski <heyheyjude23@gmail.com>
2023-03-30 10:11:57 +0200

    feat: add android ads

--- src/lib/ads/list.ts
+++ src/lib/ads/list.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native'
 import { TestIds } from 'react-native-google-mobile-ads'
 import { AdsDescription, AdsType } from './types'
 
@@ -7,17 +8,34 @@ export enum AdsName {
   GALLERY = 'GALLERY',
 }
 
+type CreateAdsIdProps = {
+  android: string
+  ios: string
+}
+
+const createAdsId = (props: CreateAdsIdProps) =>
+  Platform.select({ ...props, default: props.ios })
+
 export const ADS_LIST: Record<AdsName, AdsDescription> = {
   [AdsName.WORK_UPLOAD]: {
-    id: 'ca-app-pub-1405242860258153/9818404791',
+    id: createAdsId({
+      ios: 'ca-app-pub-1405242860258153/9818404791',
+      android: 'ca-app-pub-1405242860258153/4625410568',
+    }),
     type: AdsType.REWARDED,
   },
   [AdsName.CATEGORIES]: {
-    id: 'ca-app-pub-1405242860258153/9843305582',
+    id: createAdsId({
+      ios: 'ca-app-pub-1405242860258153/9843305582',
+      android: 'ca-app-pub-1405242860258153/4691777601',
+    }),
     type: AdsType.BANNER,
   },
   [AdsName.GALLERY]: {
-    id: 'ca-app-pub-1405242860258153/2705970029',
+    id: createAdsId({
+      ios: 'ca-app-pub-1405242860258153/2705970029',
+      android: 'ca-app-pub-1405242860258153/5719000387',
+    }),
     type: AdsType.BANNER,
   },
 }
