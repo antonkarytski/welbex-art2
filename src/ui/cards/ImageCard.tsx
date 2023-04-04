8d52f1af82f944e344c460bc6bf72fd65fa75463
antonkarytski <heyheyjude23@gmail.com>
2023-04-04 14:09:35 +0200

    refactor: doubleTap structure

--- src/ui/cards/ImageCard.tsx
+++ src/ui/cards/ImageCard.tsx
@@ -1,6 +1,5 @@
-import React, { PropsWithChildren, useRef } from 'react'
+import React, { PropsWithChildren } from 'react'
 import {
-  GestureResponderEvent,
   ImageSourcePropType,
   StyleProp,
   StyleSheet,
@@ -9,12 +8,8 @@ import {
   ViewStyle,
 } from 'react-native'
 import { defaultColors } from '../../features/themed/theme'
-import {
-  Coordinates,
-  calcDistanceBetweenCoords,
-} from '../../lib/helpers/geometry'
 import { FONT_SEMI_BOLD } from '../../styles/fonts'
-import { Timer } from '../../types'
+import { useDoubleTap } from '../doubleTouch/hooks'
 import ImageCardContent from './ImageCardContent'
 
 export type ImageOptions =
@@ -40,55 +35,6 @@ export type ImageCardProps = {
     container: StyleProp<ViewStyle>
   }
 } & ImageOptions
-
-type PrevPressData = {
-  timeout: Timer
-  coords: Coordinates
-}
-
-type UseDoubleTapProps = {
-  onPress?: () => void
-  onDoublePress?: () => void
-  delay?: number
-  maxDistance?: number
-}
-
-const DOUBLE_PRESS_DELAY = 200
-const DOUBLE_PRESS_MAX_DISTANCE = 30
-const useDoubleTap = ({
-  onPress,
-  onDoublePress,
-  delay = DOUBLE_PRESS_DELAY,
-  maxDistance = DOUBLE_PRESS_MAX_DISTANCE,
-}: UseDoubleTapProps) => {
-  const prevPressData = useRef<PrevPressData | null>(null)
-
-  return (e: GestureResponderEvent) => {
-    if (!onDoublePress) return onPress?.()
-    const coords = {
-      x: e.nativeEvent.pageX,
-      y: e.nativeEvent.pageY,
-    }
-    if (prevPressData.current) {
-      const distance = calcDistanceBetweenCoords(
-        coords,
-        prevPressData.current.coords
-      )
-      if (distance > maxDistance) return
-      clearTimeout(prevPressData.current.timeout)
-      prevPressData.current = null
-      onDoublePress?.()
-      return
-    }
-    prevPressData.current = {
-      timeout: setTimeout(() => {
-        onPress?.()
-        prevPressData.current = null
-      }, delay),
-      coords,
-    }
-  }
-}
 
 const ImageCard = ({
   style,
