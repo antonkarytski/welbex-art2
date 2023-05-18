bb3152595ca391287a67a0a51d19bc45b211cc8b
antonkarytski <heyheyjude23@gmail.com>
2023-05-18 21:54:07 +0200

    fix: remove button for downloading art work

--- src/features/artWork/ArtWorkDetails.tsx
+++ src/features/artWork/ArtWorkDetails.tsx
@@ -1,12 +1,11 @@
 import { useRequest } from '@heyheyjude/toolkit'
 import { useStore } from 'effector-react'
 import React from 'react'
-import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native'
+import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
 import { ArtWork, ArtWorkStatus } from '../../api/parts/arts/types'
 import { useNavigate } from '../../navigation'
 import { links } from '../../navigation/links'
 import { SCREEN_CONTENT_WIDTH } from '../../styles/constants'
-import { useText } from '../../translations/hook'
 import Span from '../../ui/Span'
 import { useDoubleTap } from '../../ui/doubleTouch/hooks'
 import Loader from '../../ui/loaders/Loader'
@@ -16,13 +15,11 @@ import { createThemedStyle } from '../themed'
 import { useThemedStyle } from '../themed/hooks'
 import UserCardPreview from '../user/UserCardPreview'
 import ArtWorkInteractionPanel from './ArtWorkInteractivePanel'
-import DownloadImageButton from './DownloadImageButton'
 import { useAtrWorkActions } from './hooks'
 import { getArtWorkRequest } from './request'
 
 const ArtWorkDetails = React.memo(() => {
   const navigate = useNavigate()
-  const text = useText()
   const drawing = useRequest(getArtWorkRequest)
   const myProfile = useStore($myProfile)
   const styles = useThemedStyle(themedStyles)
@@ -70,15 +67,15 @@ const ArtWorkDetails = React.memo(() => {
       )}
       <Span style={styles.title} weight={600} label={artWork.title} />
 
-      {myProfile?.subscription ? (
-        <DownloadImageButton
-          style={styles.downloadButton}
-          label={text.download}
-          artWork={artWork}
-        />
-      ) : (
-        <View style={styles.plug} />
-      )}
+      {/*{myProfile?.subscription ? (*/}
+      {/*  <DownloadImageButton*/}
+      {/*    style={styles.downloadButton}*/}
+      {/*    label={text.download}*/}
+      {/*    artWork={artWork}*/}
+      {/*  />*/}
+      {/*) : (*/}
+      {/*  <View style={styles.plug} />*/}
+      {/*)}*/}
     </ScrollView>
   )
 })
@@ -101,6 +98,7 @@ const themedStyles = createThemedStyle((colors) =>
     title: {
       color: colors.text,
       fontSize: 16,
+      marginBottom: 37, // <- remove this if getting back download button
     },
   })
 )
