954ae208c66742817937f37bb1dbdb4a79461a20
antonkarytski <heyheyjude23@gmail.com>
2023-05-18 07:53:09 +0200

    fix: available categories

--- src/features/profile/model.availableCategories.ts
+++ src/features/profile/model.availableCategories.ts
@@ -3,19 +3,10 @@ import { api } from '../../api'
 import { AvailableCategoriesResponse } from '../../api/parts/categories/types'
 
 export const setAvailableCategories = createEvent<AvailableCategoriesResponse>()
-export const $availableCategories = restore(setAvailableCategories, {
-  current_month: [2, 3, 4, 5, 6, 7],
-  next_month: [1, 2, 3, 8, 16],
-})
-
-$availableCategories.watch((e) => {
-  console.log(e)
-})
-
+export const $availableCategories = restore(setAvailableCategories, null)
 export const getAvailableCategories = attach({
   effect: api.categories.getAvailable,
 })
 getAvailableCategories.done.watch(({ result }) => {
-  console.log(result)
-  //setAvailableCategories(result)
+  setAvailableCategories(result)
 })
