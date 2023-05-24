fccb48228defab9225d9105321ff1e115efb4a6c
antonkarytski <heyheyjude23@gmail.com>
2023-05-24 06:33:23 +0200

    chore: prod version

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,9 +1,9 @@
 import { ServerManager } from '@heyheyjude/toolkit'
 
-export const ROOT_DOMAIN = 'art-sqrd.com' //'45.8.97.189'
+export const ROOT_DOMAIN = 'art-sqrd.com'
 export const DEV_ROOT_DOMAIN = `test.${ROOT_DOMAIN}`
 
 export const server = new ServerManager({
-  initialRoot: DEV_ROOT_DOMAIN,
+  initialRoot: ROOT_DOMAIN,
   apiGenerator: (root) => `https://${root}/api/v1`,
 })
