f02e393c1b14f5a8af82116d4ce7e63992e29b76
antonkarytski <heyheyjude23@gmail.com>
2023-05-26 09:09:34 +0200

    fix: winners hooks

--- src/features/winners/hooks.ts
+++ src/features/winners/hooks.ts
@@ -5,11 +5,7 @@ export const useWinnersModel = () => {
   const winners = useStore(winnersListModel.$items)
   const isNextLoading = useStore(winnersListModel.$isNextLoading)
   const isLoading = useStore(winnersListModel.$isLoading)
+  const getNextSync = () => winnersListModel.getNextSync()
 
-  return {
-    winners,
-    isNextLoading,
-    isLoading,
-    getNextSync: winnersListModel.getNextSync,
-  }
+  return { winners, isNextLoading, isLoading, getNextSync }
 }
